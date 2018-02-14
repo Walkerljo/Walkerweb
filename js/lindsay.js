54192479bd8bfcd0dbaea65aefdfc092f83fd2ef
Walkerljo <lindsay_walker@utexas.edu>
2018-02-13 22:45:00 -0600

    The Most current version of Walerweb, early Feb 2018, including updated PD for TCEA conference, and javascript on homepage

--- js/lindsay.js
+++ js/lindsay.js
@@ -1,11 +1,10 @@
-var myImage = document.querySelector('img');
+var myImage = document.getElementById('portrait');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/me.png') {
-      myImage.setAttribute ('src','images/me2.jpg');
+      myImage.setAttribute ('src','images/me_gaudi2.jpg');
     } else {
       myImage.setAttribute ('src','images/me.png');
     }
 }
-Save all files and load in
